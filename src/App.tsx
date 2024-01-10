460547253517483597be302bdb52457bab66383a
Carla Silva <carla.silva@movestock.com>
2024-01-09 22:18:56 -0300

    added toggleTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
+import { createContext, useState } from 'react'
+
 import { ThemeProvider } from 'styled-components'
 import { Router } from './routes/Router'
 import GlobalStyles from './styles/global'
 
 import light from './themes/light'
-/* import dark from "./themes/dark"; */
+import dark from './themes/dark'
+
+type Theme = typeof light
+interface ThemeContextTypes {
+  theme: Theme
+  toggleTheme: () => void
+}
+
+export const ThemeContext = createContext<ThemeContextTypes | undefined>(
+  undefined,
+)
 
 export function App() {
+  const [theme, setTheme] = useState(light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
+  const themeContextValue: ThemeContextTypes = {
+    theme,
+    toggleTheme,
+  }
+
   return (
-    <ThemeProvider theme={light}>
-      <Router />
-      <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <ThemeContext.Provider value={themeContextValue}>
+        <Router />
+        <GlobalStyles />
+      </ThemeContext.Provider>
     </ThemeProvider>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Form } from '../../components/Form'
 import { Post } from '../../types'
+import { ThemeContext } from '../../App'
 
 export function Home() {
   const [posts, setPosts] = useState<Post[]>([])
@@ -9,9 +10,16 @@ export function Home() {
     setPosts((prevState) => [...prevState, newPost])
   }
 
+  const themeContext = useContext(ThemeContext)
+
+  if (!themeContext) return null
+
+  const { toggleTheme } = themeContext
+
   return (
     <>
       {console.log(posts)}
+      <button onClick={toggleTheme}>Trocar tema</button>
       <Form onSubmitNewPost={handleCreateNewPost} />
 
       {/* listar os posts */}
